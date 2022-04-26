86811f08f8eae57d5a4ee81fa6245323d5fdba6e
Filip Graniczny <graniczn@usc.edu>
2022-04-25 21:01:13 -0700

    Refactor UserEntity getters/setters/equals/hash

--- api/src/main/java/com/csci201finalproject/triptracker/entities/UserEntity.java
+++ api/src/main/java/com/csci201finalproject/triptracker/entities/UserEntity.java
@@ -64,25 +64,16 @@ public class UserEntity {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
         UserEntity that = (UserEntity) o;
 
-        if (id != that.id)
-            return false;
-        if (name != null ? !name.equals(that.name) : that.name != null)
-            return false;
-        if (email != null ? !email.equals(that.email) : that.email != null)
-            return false;
-        if (password != null ? !password.equals(that.password) : that.password != null)
-            return false;
-        if (pfpId != null ? !pfpId.equals(that.pfpId) : that.pfpId != null)
-            return false;
-
-        return true;
+        if (id != that.id) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+        return pfpId != null ? pfpId.equals(that.pfpId) : that.pfpId == null;
     }
 
     @Override
