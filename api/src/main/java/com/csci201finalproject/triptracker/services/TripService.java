a4d444992fdfb79b004879d83153962c0d9da4dc
Filip Graniczny <graniczn@usc.edu>
2022-04-27 22:53:09 -0700

    feat: implement getTrip method in TripService

--- api/src/main/java/com/csci201finalproject/triptracker/services/TripService.java
+++ api/src/main/java/com/csci201finalproject/triptracker/services/TripService.java
@@ -1,7 +1,30 @@
 package com.csci201finalproject.triptracker.services;
 
+import com.csci201finalproject.triptracker.entities.TripEntity;
+import com.csci201finalproject.triptracker.repositories.TripRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-@Service("TripService")
+import java.util.List;
+
+@Service
 public class TripService {
+    @Autowired
+    TripRepository tripRepository;
+
+    public List<TripEntity> getTrips(int limit, String searchTerm, String sortBy, boolean ascending) {
+        Sort.Direction direction;
+        if(ascending) {
+            direction = Sort.Direction.ASC;
+        }
+        else {
+            direction = Sort.Direction.DESC;
+        }
+        Pageable pageable = PageRequest.of(0, limit, Sort.by(direction, sortBy));
+        return tripRepository.findAllByTitleContaining(searchTerm, pageable);
+    }
+
 }
