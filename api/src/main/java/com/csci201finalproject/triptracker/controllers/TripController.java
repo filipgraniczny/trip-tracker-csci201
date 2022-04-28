a31930363d77cb875e10513b04b8ef76fbd186b3
Filip Graniczny <graniczn@usc.edu>
2022-04-27 22:51:49 -0700

    feat: /explore implemented

--- api/src/main/java/com/csci201finalproject/triptracker/controllers/TripController.java
+++ api/src/main/java/com/csci201finalproject/triptracker/controllers/TripController.java
@@ -2,16 +2,35 @@ package com.csci201finalproject.triptracker.controllers;
 
 import com.csci201finalproject.triptracker.dtos.auth.LoginDTO;
 import com.csci201finalproject.triptracker.dtos.auth.RegisterDTO;
+import com.csci201finalproject.triptracker.entities.TripEntity;
 import com.csci201finalproject.triptracker.entities.UserEntity;
 import com.csci201finalproject.triptracker.interfaces.ErrorResponseClass;
-import com.csci201finalproject.triptracker.services.UserService;
+import com.csci201finalproject.triptracker.services.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Objects;
 
 @RestController
 @RequestMapping("/trips")
 public class TripController {
+    @Autowired
+    TripService tripService;
+
+    @GetMapping("/explore")
+    public @ResponseBody Object getTrips(@RequestParam(defaultValue = "10", required = false) Integer limit,
+                                         @RequestParam String searchTerm,
+                                         @RequestParam(defaultValue = "to_time", required = false) String sortBy,
+                                         @RequestParam(defaultValue = "true", required = false) boolean ascending) {
+        List<TripEntity> trips = tripService.getTrips(limit, searchTerm, sortBy, ascending);
+        if (Objects.isNull(trips) || trips.isEmpty())
+            return new ErrorResponseClass(false, "NOT_FOUND", "No trips found with this search term");
+        return trips;
+    }
+
+
+
+
 
 }
