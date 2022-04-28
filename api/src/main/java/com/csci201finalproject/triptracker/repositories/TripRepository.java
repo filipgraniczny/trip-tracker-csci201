9d7d9b438e035fa1544ec8a29ad9c9099587e0f4
Filip Graniczny <graniczn@usc.edu>
2022-04-27 22:52:27 -0700

    feat: add title search to TripRepository

--- api/src/main/java/com/csci201finalproject/triptracker/repositories/TripRepository.java
+++ api/src/main/java/com/csci201finalproject/triptracker/repositories/TripRepository.java
@@ -1,7 +1,14 @@
 package com.csci201finalproject.triptracker.repositories;
 
 import com.csci201finalproject.triptracker.entities.TripEntity;
+import com.csci201finalproject.triptracker.entities.UserEntity;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TripRepository extends CrudRepository<TripEntity, Integer> {
+    List<TripEntity> findAllByTitleContaining(String searchTerm, Pageable pageRequest);
 }
