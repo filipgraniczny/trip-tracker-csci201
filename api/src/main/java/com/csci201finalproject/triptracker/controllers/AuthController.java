0fdb1a1028f13a553f45fca9b507ddc48260575e
Vincent Vu <vinceny.fe@gmail.com>
2022-04-30 13:35:54 -0700

    fix: add cors + status on error response

--- api/src/main/java/com/csci201finalproject/triptracker/controllers/AuthController.java
+++ api/src/main/java/com/csci201finalproject/triptracker/controllers/AuthController.java
@@ -9,6 +9,8 @@ import com.csci201finalproject.triptracker.interfaces.ErrorResponseClass;
 import com.csci201finalproject.triptracker.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,16 +31,19 @@ public class AuthController {
             resultUser = userService.createUser(registerDTO);
             return resultUser;
         } catch (Exception e) { // TODO: find out what this error class is exactly
-            return new ErrorResponseClass(false, "USER_REGISTERED",
+            ErrorResponseClass error = new ErrorResponseClass(false, "USER_REGISTERED",
                     "User under this email has already been registered");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
         }
     }
 
     @PostMapping("/login")
     public @ResponseBody Object login(@RequestBody LoginDTO loginDTO) {
         UserEntity user = userService.verifyUserByEmailAndPassword(loginDTO.getEmail(), loginDTO.getPassword());
-        if (Objects.isNull(user))
-            return new ErrorResponseClass(false, "INVALID_LOGIN", "Invalid email or password");
+        if (Objects.isNull(user)) {
+            ErrorResponseClass error = new ErrorResponseClass(false, "INVALID_LOGIN", "Invalid email or password");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+        }
         return user;
     }
 }
