76a38858b505d6665d1ca44ceeb7089e80f8270d
Vincent Vu <vinceny.fe@gmail.com>
2022-04-30 13:37:23 -0700

    fix: add cors + status on error response (#89)

--- api/src/main/java/com/csci201finalproject/triptracker/TripTrackerApplication.java
+++ api/src/main/java/com/csci201finalproject/triptracker/TripTrackerApplication.java
@@ -2,10 +2,25 @@ package com.csci201finalproject.triptracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication()
 public class TripTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TripTrackerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
 	}
 }
--- api/src/main/java/com/csci201finalproject/triptracker/controllers/UserController.java
+++ api/src/main/java/com/csci201finalproject/triptracker/controllers/UserController.java
@@ -7,6 +7,8 @@ import com.csci201finalproject.triptracker.entities.UserEntity;
 import com.csci201finalproject.triptracker.interfaces.ErrorResponseClass;
 import com.csci201finalproject.triptracker.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +24,10 @@ public class UserController {
     @GetMapping("/{id}")
     public @ResponseBody Object getUserById(@PathVariable Integer id) {
         UserEntity user = userService.findUserById(id);
-        if (Objects.isNull(user))
-            return new ErrorResponseClass(false, "NOT_FOUND", "No user found with this ID");
+        if (Objects.isNull(user)) {
+            ErrorResponseClass errorResponse = new ErrorResponseClass(false, "NOT_FOUND", "No user found with this ID");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+        }
         return user;
     }
 }
