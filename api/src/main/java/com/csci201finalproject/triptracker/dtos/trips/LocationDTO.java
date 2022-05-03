6d85983baf9cff6398dd85031e2f6a45733cf833
Filip Graniczny <graniczn@usc.edu>
2022-05-02 23:09:35 -0700

    feat: add DTO getters/setters

--- api/src/main/java/com/csci201finalproject/triptracker/dtos/trips/LocationDTO.java
+++ api/src/main/java/com/csci201finalproject/triptracker/dtos/trips/LocationDTO.java
@@ -4,4 +4,28 @@ public class LocationDTO {
     private String name;
     private Double longitude;
     private Double latitude;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
 }
