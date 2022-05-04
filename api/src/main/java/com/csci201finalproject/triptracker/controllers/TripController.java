779b039a1c55fc73ff8bda88eb40809fcce62824
Vincent Vu <vinceny.fe@gmail.com>
2022-05-03 21:33:37 -0700

    fix issue with null id entities (#110)
    * fix: id issues
    
    * fix: photo id null defaults to -1

--- api/src/main/java/com/csci201finalproject/triptracker/controllers/TripController.java
+++ api/src/main/java/com/csci201finalproject/triptracker/controllers/TripController.java
@@ -1,15 +1,11 @@
 package com.csci201finalproject.triptracker.controllers;
 
-import com.csci201finalproject.triptracker.dtos.auth.LoginDTO;
-import com.csci201finalproject.triptracker.dtos.auth.RegisterDTO;
 import com.csci201finalproject.triptracker.entities.TripEntity;
-import com.csci201finalproject.triptracker.entities.UserEntity;
 import com.csci201finalproject.triptracker.interfaces.ErrorResponseClass;
 import com.csci201finalproject.triptracker.services.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.Objects;
 
@@ -21,9 +17,9 @@ public class TripController {
 
     @GetMapping("/explore")
     public @ResponseBody Object getTrips(@RequestParam(defaultValue = "10", required = false) Integer limit,
-                                         @RequestParam String searchTerm,
-                                         @RequestParam(defaultValue = "toTime", required = false) String sortBy,
-                                         @RequestParam(defaultValue = "true", required = false) boolean ascending) {
+            @RequestParam String searchTerm,
+            @RequestParam(defaultValue = "toTime", required = false) String sortBy,
+            @RequestParam(defaultValue = "true", required = false) boolean ascending) {
         List<TripEntity> trips = tripService.getTrips(limit, searchTerm, sortBy, ascending);
         if (Objects.isNull(trips) || trips.isEmpty())
             return new ErrorResponseClass(false, "NOT_FOUND", "No trips found with this search term");
